c0a19e0425c2cf3aa96a4688e2de8707f9661af6
CJ42 <cavallerajean@gmail.com>
2024-02-22 13:06:59 +0000

    build: upgrade dependencies and style with `@lukso/web-components`

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
+const { copyAssets } = require("@lukso/web-components/tools/copy-assets.cjs");
+// @ts-ignore
+const { assets } = require("@lukso/web-components/tools/assets/index.cjs");
+
+copyAssets("./public", assets);
+
 module.exports = {
   reactStrictMode: true,
   async rewrites() {
     return [
       // Rewrite everything else to use `pages/index`
       {
-        source: '/:path*',
-        destination: '/',
+        source: "/:path*",
+        destination: "/",
       },
     ];
   },
-}
+};
